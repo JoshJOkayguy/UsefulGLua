631a6591451c6cfbd5ac8be9ebf3f262d4dc298b
JoshJOkayguy <55960406+JoshJOkayguy@users.noreply.github.com>
2021-02-22 14:57:02 -0500

    Update readme.lua

--- readme.lua
+++ readme.lua
@@ -1,1 +1,10 @@
-
+-- Cool derma opening "popup" animation
+-- [Clientside] --
+local function PopupAnimation()
+  
+end
+-- Get the local player's FPS
+-- [Clientside] --
+local function LocalPlayerFPS()
+  
+end
