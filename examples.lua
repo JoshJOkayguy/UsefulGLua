ef6a41134e4fbfaf81c56febf4815a4c0669fa04
JoshJOkayguy <55960406+JoshJOkayguy@users.noreply.github.com>
2021-02-22 15:42:06 -0500

    Update examples.lua

--- examples.lua
+++ examples.lua
@@ -40,7 +40,7 @@ local function sqlExplanation()
 -- The use of SQLite in gmod is actually pretty simple once you get the hang of it. But, you don't want to overuse it. Database tables are useful because, unlike lua tables (if you don't know what those are, you are not ready for this), they carry data over between restarts and from server to server. It's just like a table except you can't see it in your code all in one block, you're creating "queries" to access it to store and remove data.
   
   local exampleVar = "I love SQL"
-  if !sql.TableExists( 'UsefulGLua_sqlTutorial' ) then
+  if !sql.TableExists( 'UsefulGLua_sqlTutorial' ) then -- In case you missed it, we are checking if the table does NOT exist, look at the exclamation mark prior to the statement.
     
     sql.Query("CREATE TABLE UsefulGLua_sqlTutorial(column1 VARCHAR(255), column2 VARCHAR(255) ) ") -- Creates a table called "UsefulGLua_sqlTutorial" with two columns named column1 and column2. VARCHAR is simply an identifier of the datatype that will go into that column.
     sql.Query("INSERT INTO UsefulGLua_sqlTutorial('column1' 'column2')VALUES ('"..exampleVar.."', 'SQL is cool') ") -- Inserts the value stored in exampleVar into column1 and "SQL is cool" into column2. As you can see, you have to escape the quotes that SQLite needs to concatenate a variable.
